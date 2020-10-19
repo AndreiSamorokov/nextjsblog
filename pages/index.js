b8f7d12b1632c63dd51569a48ee4cd855779cec6
AndreiSamorokov <andreiv0202@gmail.com>
2020-10-20 04:18:54 +0900

    added menu

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Head from 'next/head'
 
 export default function Home() {
@@ -6,46 +7,31 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <ul className="nav_link">
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About Us</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/news">
+              <a>Blog Post</a>
+            </Link>
+          </li>
+        </ul>
       </Head>
 
       <main>
         <h1 className="title">
           Welcome to My Area
         </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+ 
+         
       </main>
 
       <footer>
@@ -59,6 +45,9 @@ export default function Home() {
       </footer>
 
       <style jsx>{`
+        .nav_link li{
+          float: left; clear: left; margin-bottom: 10px; background: #ccc
+        }
         .container {
           min-height: 100vh;
           padding: 0 0.5rem;
