565f673dfe775ae25575acdc8818220a1bdc0024
AndreiSamorokov <andreiv0202@gmail.com>
2020-11-06 06:43:55 +0900

    url&key

--- pages/newblog.js
+++ pages/newblog.js
@@ -1,6 +1,11 @@
-function NewBlog() {
+import Layout from "../components/Layout";
+
+function NewBlog({url}) {
     return(
+      <Layout>
         <div>new blog page</div>
+        <div>Your Search Keyword IS{url.query.keyword}</div>
+      </Layout>
     );
   }
   
